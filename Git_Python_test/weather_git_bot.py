37f2decfbfe050680d016a109fae763360a7d705
A-F-git-lab <fisher114a@gmail.com>
2025-01-25 11:50:16 +0200

    weeather_1.01

---
+++ Git_Python_test/weather_git_bot.py
@@ -0,0 +1,15 @@
+import pyowm
+from pyowm import OWM
+from pyowm.utils import config
+from pyowm.utils import timestamps
+
+owm = pyowm.OWM('d1aff35b44e2555c08137cee836ba064')
+mgr = owm.weather_manager()
+user = input("Введите город: ")
+observation = mgr.weather_at_place(user)
+w = observation.weather
+
+print(f"Статус погоды: {w.detailed_status}")
+print(f"Температура: {w.temperature('celsius')['temp']}°C")
+print(f"Скорость ветра: {w.wind()['speed']} м/с")
+print(f"Влажность: {w.humidity}%")
